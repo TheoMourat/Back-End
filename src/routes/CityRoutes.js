9f5819d499d0b3b84c6ce5edf6ff3193a1ab9dc0
NikosKourkoulos1 <38915607+NikosKourkoulos1@users.noreply.github.com>
2024-03-19 12:22:36 +0200

    added cityEvents route

--- src/routes/CityRoutes.js
+++ src/routes/CityRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const City = require('../models/City'); 
 const router = express.Router();
 
-// //api endpoint that gets all cities
+//api endpoint that gets all cities
 router.get('/', async (req, res) => {
     try {
         const cities = await City.find();
@@ -12,6 +12,19 @@ router.get('/', async (req, res) => {
     }
 });
 
-// more event api-endpoints should be added here
-
+// get a specific city events
+router.get('/:cityId/events', async (req, res) => {
+    try {
+        const cityId = req.params.cityId;
+        const cityEvents = await City.findById(cityId).populate('events');
+        if (!cityEvents) {
+            return res.status(404).send('City not found');
+        }
+        console.log("City with Events:", cityEvents);
+        res.json(cityEvents);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching city events');
+    }
+})
 module.exports = router;
